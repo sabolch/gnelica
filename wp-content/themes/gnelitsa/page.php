8851b21a16917dc9fab50f02e8e3a6273ae23375
Konstantin Semenov <kvsemenoff@mail.ru>
2015-11-30 15:31:05 +0300

    зесь я добавил поиск

--- wp-content/themes/gnelitsa/page.php
+++ wp-content/themes/gnelitsa/page.php
@@ -1,24 +1,22 @@
-	<?php get_header(); ?>
+<?php get_header(); ?>
 
-	<div class="container-fluid jo_container_fluid">
-        <div class="container">
-            <div class="row">
-                <?php include_once ('breadcramp.php') ?>
-                <div class="jo_big_title col-md-12">
-                    <h1><?php the_title(); ?></h1>
-                    <hr>
-                </div>
-                
-                
-                <?php while ( have_posts() ) : the_post(); ?>
-                <?php the_content(); ?>        
-                <?php endwhile; ?>
-
-                <hr class=jos_dash>
-                <?php include_once('social.php') ?>
+<div class="container-fluid jo_container_fluid">
+    <div class="container">
+        <div class="row">
+            <?php include_once ('breadcramp.php') ?>
+            <div class="jo_big_title col-md-12">
+                <h1><?php the_title(); ?></h1>
+                <hr>
             </div>
+            
+            <?php while ( have_posts() ) : the_post(); ?>
+            <?php the_content(); ?>        
+            <?php endwhile; ?>
+
+            <hr class=jos_dash>
+            <?php include_once('social.php') ?>
         </div>
     </div>
-					
+</div>
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
